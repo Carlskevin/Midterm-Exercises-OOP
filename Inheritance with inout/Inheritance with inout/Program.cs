316171d7206752462f583413f8008bb24db6a701
Carlskevin <carlskevinb@gmail.com>
2025-09-09 22:39:25 +0800

    Add files via upload
    Inheritance with user input

---
+++ Inheritance with inout/Inheritance with inout/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance_with_inout
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Carls Kevin Belleza
+            //SECTION: IT306
+            //
+
+            // Call method to get sports car details    
+            Console.Write("Enter sports car color: ");
+            string sportsCarColor = Console.ReadLine();
+            Console.Write("Enter sports car model: ");
+            string sportsCarModel = Console.ReadLine();
+            Console.Write("Enter sports car year: ");
+            int sportsCarYear = int.Parse(Console.ReadLine());
+            Console.Write("Enter sports car top speed: ");
+            int topSpeed = int.Parse(Console.ReadLine());
+
+            SportsCar sportsCar = new SportsCar(sportsCarColor, sportsCarModel, sportsCarYear, topSpeed);
+            sportsCar.SportsCarInfo();
+
+            Console.ReadKey();
+        }
+    }
+}
