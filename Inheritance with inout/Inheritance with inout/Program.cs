e1cf0e30f1a7222c5792fe1cd0c22caafa0e4dc2
Carlskevin <carlskevinb@gmail.com>
2025-09-10 14:53:06 +0800

    Update Program.cs

--- Inheritance with inout/Inheritance with inout/Program.cs
+++ Inheritance with inout/Inheritance with inout/Program.cs
@@ -12,8 +12,17 @@ namespace Inheritance_with_inout
         {
             //Carls Kevin Belleza
             //SECTION: IT306
-            //
-
+            //EXER_08: Inheritance with User Input
+            
+           Console.WriteLine("Pick the car you want: ");
+           Console.WriteLine("\n1.SportsCar \n2.PickUpCar");
+           Console.WriteLine("\nSelect the Car you want: ");
+           int choice = Convert.ToInt32(Console.ReadLine());
+       if (choice==1)
+       {
+               Console.Clear();
+               Console.WriteLine("");
+       }
             // Call method to get sports car details    
             Console.Write("Enter sports car color: ");
             string sportsCarColor = Console.ReadLine();
@@ -23,7 +32,7 @@ namespace Inheritance_with_inout
             int sportsCarYear = int.Parse(Console.ReadLine());
             Console.Write("Enter sports car top speed: ");
             int topSpeed = int.Parse(Console.ReadLine());
-
+            
             SportsCar sportsCar = new SportsCar(sportsCarColor, sportsCarModel, sportsCarYear, topSpeed);
             sportsCar.SportsCarInfo();
 
@@ -31,3 +40,4 @@ namespace Inheritance_with_inout
         }
     }
 }
+
