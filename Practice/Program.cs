d7b44553619d77a0f3bb0b31ae5fc48f5a2a4a8a
Carlskevin <carlskevinb@gmail.com>
2025-09-15 14:36:45 +0800

    Add files via upload
    Inheritance with computation

---
+++ Practice/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Tshirt sando = new Sando { Price = 3500, Discount = 500 };
+            Tshirt polo = new Polo { Price = 5000, Discount = 600 };
+            Tshirt croptop = new Croptop { Price = 1000, Discount = 70 };
+
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("\t\t\tTshirt Price");
+            Console.ResetColor();
+            Console.WriteLine($"\nSando Price after Discount: {sando.DiscountedPrice}");
+            Console.WriteLine($"Polo Price after Discount: {polo.DiscountedPrice}");
+            Console.WriteLine($"Croptop Price after Discount: {croptop.DiscountedPrice}");
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Practice/Tshirt.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+    internal class Tshirt
+    {
+        public double Price { get; set; }
+        public double Discount { get; set; }
+
+        public virtual double DiscountedPrice
+        {
+            get { return Price - DiscountedPrice; }
+        }
+    }
+
+    class Sando : Tshirt
+    {
+        public override double DiscountedPrice
+        {
+            get { return Price - Discount; }
+        }
+    }
+    class Polo : Tshirt
+    {
+        public override double DiscountedPrice
+        {
+            get { return Price - Discount; }
+        }
+    }
+    class Croptop : Tshirt
+    {
+        public override double DiscountedPrice
+        {
+            get { return Price - Discount; }
+        }
+    }
+}
